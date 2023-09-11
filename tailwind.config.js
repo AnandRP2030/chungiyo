225a5883de8fa0b4cff0db109e3d5de28b07bf16
AnandRP2030 <anand.rp2030@gmail.com>
2023-09-12 00:04:13 +0530

    navbar animation work completed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'body-background': '#88b2c4' ,
+      'black': '#000000'
+    }
+  },
+  plugins: [],
+}
